1656595034c0e5373d477b33ce650b1ab0bde191
Zero <zeronbo@163.com>
2014-07-28 18:19:10 +0800

    增加测试代码的注释信息

--- src/test/java/jpinyin/opensource/jpinyin/TestPinyinHelper.java
+++ src/test/java/jpinyin/opensource/jpinyin/TestPinyinHelper.java
@@ -17,11 +17,12 @@ public class TestPinyinHelper {
         //带声调
         System.out.println(PinyinHelper.convertToPinyinString("重", ",",
                 PinyinFormat.WITH_TONE_MARK));
-
+        //用数字表示声调
         System.out.println(PinyinHelper.convertToPinyinString("重", ",",
                 PinyinFormat.WITH_TONE_NUMBER));
 
         System.out.println("============================");
+        //将单个字符转换为拼音，返回结果为String[]，数组长度>=2表示此字符为多音字
         String[] str = PinyinHelper.convertToPinyinArray('重', PinyinFormat.WITHOUT_TONE);
         for (String s : str) {
             System.out.println(s);
